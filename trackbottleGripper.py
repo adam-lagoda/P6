56d3ac6c6197c74eae10aea66ba30fd7c4bd8a81
adam-lagoda <adam.lagoda.w@gmail.com>
2022-05-01 14:03:42 +0200

    gripper2

--- trackbottleGripper.py
+++ trackbottleGripper.py
@@ -1,17 +1,4 @@
-#################################################
-# Visual Servoing Robotic Arm
-# Created by: AL, TAP, SFMS
-# Hardware:
-#   Kinova Kortex Lite Gen 3
-#   Intel L515
-#################################################
-#settings
-
-KP=0.1
-KI=100
-KD=50
-
-#################################################
+from charset_normalizer import detect
 import pyrealsense2 as rs
 import numpy as np
 import cv2
@@ -20,6 +7,18 @@ import time
 import sys
 import os
 import threading
+
+P_pixel_Width = 94      #Pixel of object with distance D_initial #43 for fisheye
+D_known_distance = 40   #Measured distance from camera to object in cm #52 for fishye
+W_Known_width = 5.7     #Known width of object in cm
+
+def focal_length(pixelWidth, knownDistance, knownWidth):
+    focalLength = (pixelWidth * knownDistance) / knownWidth
+    return focalLength
+
+def distance_to_camera (focalLength, knownWidth, pixelWidth):
+    distance = (knownWidth * focalLength) / pixelWidth
+    return distance
 
 from time import perf_counter_ns
 
@@ -59,6 +58,57 @@ def sendSpeed(base, speeds):
     twist.angular_z = speeds[5]
 
     base.SendTwistCommand(command)
+class GripperCommandExample:
+    def __init__(self, router, proportional_gain = 2.0):
+
+        self.proportional_gain = proportional_gain
+        self.router = router
+
+        # Create base client using TCP router
+        self.base = BaseClient(self.router)
+
+
+    def open_gripper(self):
+        # Create the GripperCommand we will send
+        gripper_command = Base_pb2.GripperCommand()
+        finger = gripper_command.gripper.finger.add()
+
+        gripper_command.mode = Base_pb2.GRIPPER_SPEED
+        finger.value = 0.1
+        self.base.SendGripperCommand(gripper_command)
+        gripper_request = Base_pb2.GripperRequest()    
+
+        # Wait for reported position to be opened
+        gripper_request.mode = Base_pb2.GRIPPER_POSITION
+        while True:
+            gripper_measure = self.base.GetMeasuredGripperMovement(gripper_request)
+            if len (gripper_measure.finger):
+                print("Current position is : {0}".format(gripper_measure.finger[0].value) + "4444444444444444444444")
+                if gripper_measure.finger[0].value < 0.01:
+                    break
+            else: # Else, no finger present in answer, end loop
+                break
+
+    def close_gripper(self):
+        # Create the GripperCommand we will send
+        gripper_command = Base_pb2.GripperCommand()
+        finger = gripper_command.gripper.finger.add()            
+
+        gripper_command.mode = Base_pb2.GRIPPER_SPEED
+        finger.value = -0.1
+        self.base.SendGripperCommand(gripper_command)
+        gripper_request = Base_pb2.GripperRequest()    
+        # Wait for reported speed to be 0
+        gripper_request.mode = Base_pb2.GRIPPER_SPEED
+        while True:
+            gripper_measure = self.base.GetMeasuredGripperMovement(gripper_request)
+            if len (gripper_measure.finger):
+                print("Current speed is : {0}".format(gripper_measure.finger[0].value) + "66666666666666666666666")
+                if gripper_measure.finger[0].value == 0.0:
+                    break
+            else: # Else, no finger present in answer, end loop
+                break
+
 
 def example_move_to_home_position(base):
     # Make sure the arm is in Single LEvel Servoing Mode
@@ -98,22 +148,59 @@ def example_move_to_home_position(base):
         print("Timeout on action notification wait")
     return finished
 
+#----------------------------------
+def example_move_to_home_position1(base):
+    # Make sure the arm is in Single LEvel Servoing Mode
+    base_servo_mode = Base_pb2.ServoingModeInformation()
+    base_servo_mode.servoing_mode = Base_pb2.SINGLE_LEVEL_SERVOING
+    base.SetServoingMode(base_servo_mode)
+
+    # Move arm to ready position
+    print("Moving arm to a safe position")
+    action_type = Base_pb2.RequestedActionType()
+    action_type.action_type = Base_pb2.REACH_JOINT_ANGLES
+    action_list = base.ReadAllActions(action_type)
+    action_handle = None
+    for action in action_list.action_list: #search through the library of saved movement
+        if action.name == "Home position":#bottlehome
+            action_handle = action.handle
+            
+    if action_handle == None:
+        print("Can't reach safe position. Not in the list. Exiting")
+        sys.exit(0)
+    
+    e = threading.Event()
+    notification_handle = base.OnNotificationActionTopic(
+        check_for_end_or_abort(e),
+        Base_pb2.NotificationOptions()
+    )
+    
+    base.ExecuteActionFromReference(action_handle)
+    
+    #Leave time to action to complete
+    finished = e.wait(TIMEOUT_DURATION)
+    base.Unsubscribe(notification_handle)
+    
+    if finished:
+        print("Safe position reached")
+    else:
+        print("Timeout on action notification wait")
+    return finished
+#-------------------------------------
+
+
 # Configure depth and color streams
 pipeline = rs.pipeline()
 config = rs.config()
 
 follow = False
 moveTowards = False
-startTime = perf_counter_ns()
-prevLoopTime = perf_counter_ns()
-integral_X=0
-integral_Y=0
-derivative_X=0
-derivative_Y=0
-PIDoutput_X=0
-PIDoutput_X=0
-prevError_X=0
-prevError_Y=0
+closeToObject = False
+gripperClosed = False
+detectTime = perf_counter_ns()
+lastDetectTime = perf_counter_ns()
+loopTime = perf_counter_ns()
+lastLoopTime = perf_counter_ns()
 
 config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
 
@@ -130,13 +217,17 @@ args = utilities.parseConnectionArguments()
 with utilities.DeviceConnection.createTcpConnection(args) as router:
     base = BaseClient(router)
     base_cyclic = BaseCyclicClient(router)
+    example_move_to_home_position1(base)
+    time.sleep(5)
     example_move_to_home_position(base)
+    example = GripperCommandExample(router)
+    example.open_gripper()
     time.sleep(5)
     try:
         while True:
-            timeDelta = (perf_counter_ns() - prevLoopTime) / 1e9  #[sec]
-            prevLoopTime = perf_counter_ns()
-            secondsSinceStart = (perf_counter_ns() - startTime) / 1e9
+            loopTime = perf_counter_ns()
+            loopRate = (loopTime - lastLoopTime)/ 1e9
+            lastLoopTime = loopTime
             st = time.time()
             # Wait for coherent pair of frames: depth and color
             frames = pipeline.wait_for_frames()
@@ -165,47 +256,37 @@ with utilities.DeviceConnection.createTcpConnection(args) as router:
             objs_name = objs.loc[objs['name'] == 'bottle'] #people
             
             try:
-                #detectTime = perf_counter_ns()
+                detectTime = perf_counter_ns()
                 obj = objs_name.iloc[0]
                 middle_x = (obj.xmax-obj.xmin)/2 + obj.xmin
                 middle_y = (obj.ymax-obj.ymin)/2 + obj.ymin
                 xdist = middle_x-width/2
                 ydist = middle_y-height/2
-                #print('xdist: ' + str(round(xdist,2)) + '\t' + 'ydist: ' + str(round(ydist,2)))
-            #    anglex, angley = get_angles(int(middle_x), int(middle_y), width, height)
-            #    dist = round(depth_image[int(middle_y)][int(middle_x)] * depth_scale,2)
-            #    x,y,z = get_cart(anglex, angley, dist)
-                #detectRate = (detectTime-lastDetectTime)/ 1e9
-                #lastDetectTime=detectTime
-                print('xdist: ' + str(round(xdist,2)) + '\t' + 'ydist: ' + str(round(ydist,2)) + '\t' + "timeDelta: " + str(timeDelta))
+
+                F = focal_length(P_pixel_Width, D_known_distance, W_Known_width)
+                W_object_width = obj.xmax-obj.xmin
+                D = distance_to_camera(F, W_Known_width, W_object_width)
+
+
+
+                detectRate = (detectTime-lastDetectTime)/ 1e9
+                lastDetectTime=detectTime
+                print('xdist: ' + str(round(xdist,2)) + '\t' + 'ydist: ' + str(round(ydist,2)) + '\t' + 'object width:' + str(W_object_width) + '\t' + 'Distance:' + str(D) + '\t' + 'Focal Length:' + (str(F)))    
                 
                 cv2.rectangle(color_image, (int(obj.xmin), int(obj.ymin)), (int(obj.xmax), int(obj.ymax)), (0,255,0),2)
                 cv2.circle(color_image, (int(middle_x), int(middle_y)), 5, (0, 255, 0), 2)
                 cv2.circle(color_image, (int(width/2), int(height/2)), 5, (0, 0, 255), 2)
                 cv2.line(color_image, (int(middle_x), int(middle_y)), (int(width/2), int(height/2)), (0,0,255), 2)
 
-                 #PID Controller X
-                target_X = 0
-                error_X = target_X - xdist
-                integral_X += error_X * timeDelta
-                derivative_X = (error_X - prevError_X) / timeDelta
-                prevError_X = error_X
-                PIDoutput_X = KP * error_X + KI * integral_X + KD * derivative_X
-                
-                #PID Controller Y
-                target_Y = 150
-                error_Y = target_Y - ydist
-                integral_Y += error_Y * timeDelta
-                derivative_Y = (error_Y - prevError_Y) / timeDelta
-                prevError_Y = error_Y
-                PIDoutput_Y = KP * error_Y + KI * integral_Y + KD * derivative_Y
-                print("ok")
+                #if(abs(xdist) < 10 and abs(ydist) < 10):
+                    #xdist = 0
+                    #ydist = 0
+                deadband = 2    
                 yOffset = 150
                 Kp = 0.1
                 speedx = -Kp * xdist
-                #speedy = -Kp * (ydist-yOffset)
-                speedy = 0
-                deadband = 2 
+                speedy = -Kp * (ydist-yOffset)
+                
                 if(speedx > deadband):
                     speedx = deadband
                 if(speedx < -deadband):
@@ -215,17 +296,33 @@ with utilities.DeviceConnection.createTcpConnection(args) as router:
                 if(speedy < -deadband):
                     speedy = -deadband
                 #if(abs(xdist) > 7 and abs(ydist) > 7):
-                #if(abs(xdist) > 5 or abs(ydist) > 165 or abs(ydist) < 135):
-                #speeds = [speedx/50, speedy/50, 0.01, -speedy*2, speedx*2, 0]
-
-                if(moveTowards == True):
-                    speeds = [speedx/50, speedy/50, 0.01, -speedy*2, speedx*2, 0]
+                if(D > 15):
+                    closeToObject = False
+                    if(abs(xdist) > 5 or abs(ydist) > 165 or abs(ydist) < 135):
+                        speeds = [speedx/100, speedy/100, 0, -speedy*2, speedx*2, 0]
+                        sendSpeed(base, speeds)
+                    else:
+                        #speeds = [speedx/50, speedy/50, 0.05, -speedy*2, speedx*2, 0]                        
+                        speeds = [0, 0, 0.05, 0, 0, 0]
+                        sendSpeed(base, speeds)                       
                 else:
-                    speeds = [speedx/50, speedy/50, 0, -speedy*2, speedx*2, 0]
-                
-                sendSpeed(base, speeds)
+                    closeToObject = True
+                    #example.ExampleSendGripperCommands()
+                if closeToObject:
+                    example = GripperCommandExample(router)
+                    speeds = [0, 0, 0, 0, 0, 0]
+                    sendSpeed(base, speeds)   
+                    #example.close_gripper()
+                    #time.sleep(5)
+                    gripperClosed = True
+                if closeToObject and gripperClosed:
+                    time.sleep(5)
+                    example_move_to_home_position(base)
+                    time.sleep(5)
+                    #example = GripperCommandExample(router)
+                    #example.open_gripper()
+                    sendSpeed(base, speeds)
 
-                
                 pxheight = obj.ymax - obj.ymin
                 pxwidth = obj.xmax - obj.xmin
                 heightratio = pxheight/height
