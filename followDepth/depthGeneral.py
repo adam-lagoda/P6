16c3592fcc5c637b9ee0a549ee2a04d59e21f616
adam-lagoda <adam.lagoda.w@gmail.com>
2022-05-20 13:23:52 +0200

    working depth

--- followDepth/depthGeneral.py
+++ followDepth/depthGeneral.py
@@ -8,30 +8,30 @@ import time
 import torch
 from realsense_depth import *
 
-#point = (400, 300)
+point = (400, 300)
 
-#def show_distance(event, x, y, args, params):
-#    global point
-#    point = (x, y)
+def show_distance(event, x, y, args, params):
+    global point
+    point = (x, y)
 
 # Initialize Camera Intel Realsense
 dc = DepthCamera()
 
-model = torch.hub.load('ultralytics/yolov5', 'yolov5n', pretrained=True)
-#model = torch.hub.load('ultralytics/yolov5', 'custom', path='path/to/best1.pt')
+#model = torch.hub.load('ultralytics/yolov5', 'yolov5n', pretrained=True)
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='path/to/best.pt')
 time.sleep(5)  
 print('Model has been downloaded and created') 
 
 # Create mouse event
-#cv2.namedWindow("Color frame")
-#cv2.setMouseCallback("Color frame", show_distance)
+cv2.namedWindow("Color frame")
+cv2.setMouseCallback("Color frame", show_distance)
 
 while True:
-    ret, depth_frame, color_frame = dc.get_frame()
+    ret, depth_frame, color_frame, depth_image = dc.get_frame()
 
     result = model(color_frame)
     objs = result.pandas().xyxy[0]
-    objs_name = objs.loc[objs['name'] == 'bottle']
+    objs_name = objs.loc[objs['name'] == 'weed']
     
     height = color_frame.shape[0]
     width = color_frame.shape[1]
@@ -51,17 +51,26 @@ while True:
         cv2.circle(color_frame, (int(width/2), int(height/2)), 5, (0, 0, 255), 2)
         cv2.line(color_frame, (int(x_middle), int(y_middle)), (int(width/2), int(height/2)), (0,0,255), 2)
 
-        distance = depth_frame[int(y_middle), int(x_middle)]/10 
+        ret, depth_aligned_frame, color_aligned_frame, distance_frame_depth = dc.get_aligned_frame()
+
+        distance = distance_frame_depth.get_distance(int(x_middle), int(y_middle))
+        distance = round(distance*100, 2)
         print("Distance: ", distance)
+        #depth = depth_frame[int(x_middle), int(y_middle)] 
+        #dx ,dy, dz = rs.rs2_deproject_pixel_to_point(depth_frame, [x_middle,y_middle], depth)
+        #distance = math.sqrt(((dx)**2) + ((dy)**2) + ((dz)**2))
+        #print("Distance: ", distance)
     except:
         print("Object not detected")
     # Show distance for a specific point
     #cv2.circle(color_frame, point, 4, (0, 0, 255))
-    #distance = depth_frame[point[1], point[0]]
     #distance = depth_frame[x_middle, y_middle]
     #print("Distance: ", distance)
     #cv2.putText(color_frame, "{}mm".format(distance), (point[0], point[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
-
+    
+    
+    
+    
     cv2.imshow("depth frame", depth_frame)
     cv2.imshow("Color frame", color_frame)
     key = cv2.waitKey(1)
--- followDepth/realsense_depth.py
+++ followDepth/realsense_depth.py
@@ -13,11 +13,15 @@ class DepthCamera:
         device = pipeline_profile.get_device()
         device_product_line = str(device.get_info(rs.camera_info.product_line))
 
-        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        config.enable_stream(rs.stream.depth, 1024, 768, rs.format.z16, 30) #1024x768
+        config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30) #640 480 for both
 
         # Start streaming
         self.pipeline.start(config)
+        
+        #align
+        self.align_to = rs.stream.color
+        self.align = rs.align(self.align_to)
 
     def get_frame(self):
         frames = self.pipeline.wait_for_frames()
@@ -27,8 +31,24 @@ class DepthCamera:
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
         if not depth_frame or not color_frame:
-            return False, None, None
-        return True, depth_image, color_image
+            return False, None, None, None
+        return True, depth_image, color_image, depth_frame
 
+    def get_aligned_frame(self):
+        frames = self.pipeline.wait_for_frames()
+        aligned_frames = self.align.process(frames)
+        
+        # Get aligned frames
+        aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+        color_frame = aligned_frames.get_color_frame()
+        
+        depth_image = np.asanyarray(aligned_depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+        
+        
+        if not aligned_depth_frame or not color_frame:
+            return False, None, None
+        return True, depth_image, color_image, aligned_depth_frame
+        
     def release(self):
         self.pipeline.stop()
